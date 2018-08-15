fc7f510b0cf3d202a2be1bef5b0d99f3cbfd8fd8
maxiaoyao <maxr@polysoft.com.cn>
2018-08-15 13:28:36 +0800

    底部栏

--- js/pages/HomePage/index.js
+++ js/pages/HomePage/index.js
@@ -1,12 +1,19 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {View,Text} from 'react-native'
+import Icon from 'react-native-vector-icons/Ionicons';
 
 
 class home extends Component {
+  static navigationOptions = {
+    tabBarLabel: '主页',
+    tabBarIcon: ({focused}) =>
+      (focused ? <Icon name={'ios-home'} size={18}  color={'black'}/> :
+        <Icon name={'ios-home'} size={18}  color={'gray'}/>),
+  }
     render() {
         return (
-            <View style={{justifyContent:'center', alignItems:'center'}}>
+            <View style={{flex:1,justifyContent:'center', alignItems:'center'}}>
                 <Text>home</Text>
             </View>
         );
--- js/pages/MePage/index.js
+++ js/pages/MePage/index.js
@@ -1,14 +1,22 @@
 import React, {Component} from 'react';
-import {View,Text} from 'react-native'
-import {connect} from 'react-redux';
+import {View, Text} from 'react-native'
+import Icon from 'react-native-vector-icons/Ionicons';
+
 class MeScreen extends Component {
-    render() {
-        return (
-            <View  style={{justifyContent:'center', alignItems:'center'}}>
-               <Text>MeScreen</Text>
-            </View>
-        );
-    }
+  static navigationOptions = {
+    tabBarLabel: '我的',
+    tabBarIcon: ({focused}) =>
+      (focused ? <Icon name={'ios-body'} size={22} color={'black'}/> :
+      <Icon name={'ios-body'} size={22}  color={'gray'}/>),
+  }
+
+  render() {
+    return (
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <Text>Me</Text>
+      </View>
+    );
+  }
 }
 
 
